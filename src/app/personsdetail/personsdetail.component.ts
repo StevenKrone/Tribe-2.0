ad8a61a17dcb7acc9c8c484ab2a94eec1ded1b91
Steven Krone <steven.krone@us.sogeti.com>
2017-07-25 14:32:31 -0500

    Styling changes

--- src/app/personsdetail/personsdetail.component.ts
+++ src/app/personsdetail/personsdetail.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Person } from '../_class/person';
+import { PersonService } from '../_services/person.service';
+
+//import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute} from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+
 
 @Component({
   selector: 'app-personsdetail',
@@ -6,10 +17,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./personsdetail.component.css']
 })
 export class PersonsdetailComponent implements OnInit {
+  @Input() person: Person;
+  private sub: any;
 
-  constructor() { }
+
+
+  constructor(
+        private personService: PersonService,
+        private route: ActivatedRoute,
+        private location: Location
+  ) { }
 
   ngOnInit() {
+  //Hard-Coded 
+    this.person = { id: 11, name: 'Steven' };
+
+
+  //Option from StackOverflow
+    // this.sub = this.route
+    // .params
+    // .subscribe(params => { 
+    //   this.person = params['id'];
+    // })
+
+  //Option 2 from angular-tour-of-heroes
+    //  this.route.paramMap
+    // .switchMap((params: ParamMap) => this.personService.getPerson(+params.get('id')))
+    // .subscribe(person => this.person = person);
   }
+
+    goBack(): void {
+    this.location.back();
+}
+
+save(): void {
+  this.personService.update(this.person)
+    .then(() => this.goBack());
+}
 
 }
